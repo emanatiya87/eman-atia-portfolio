de1aba28f14b1e45ec78c28843a391f08013199c
eman-atia <eman.atiya@ecs-co.com>
2026-08-17 09:43:13 +0300

    create icon contain EA

---
+++ app/apple-icon.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+export const size = { width: 180, height: 180 };
+export const contentType = "image/png";
+
+export default function AppleIcon() {
+  return new ImageResponse(
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#0A0F1E",
+      }}
+    >
+      <span
+        style={{
+          fontSize: 84,
+          fontWeight: 700,
+          color: "#4CD9E8",
+          fontFamily: "monospace",
+        }}
+      >
+        EA
+      </span>
+    </div>,
+    { ...size },
+  );
+}
